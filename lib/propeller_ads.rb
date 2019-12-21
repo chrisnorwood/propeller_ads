b6016e8660728083d19452e76c7659af9c46db8b
Chris Norwood <cnorwood92@gmail.com>
2019-12-20 17:25:43 -0800

    Intialize new repo

---
+++ lib/propeller_ads.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "propeller_ads/version"
+
+module PropellerAds
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/propeller_ads_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PropellerAds do
+  it "has a version number" do
+    expect(PropellerAds::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
